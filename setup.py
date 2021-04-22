1a6ea6c5b96b8d3bfee871c2b08c36e2b7620b7a
Stefan Heid <stefan-heid@web.de>
2021-04-22 21:36:27 +0200

    added playsound to dependencies

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     version=pysplit.__version__,
     packages=['pysplit', 'pysplit.client'],
     install_requires=['requests>=2.20.0', 'PyYAML>=5.3.1',
-                      'Flask>=1.1.2', 'tornado>=5.1'],
+                      'Flask>=1.1.2', 'tornado>=5.1', 'playsound>=1.2'],
     entry_points={
         'console_scripts': ['pysplit = pysplit.runner:main']
     }
