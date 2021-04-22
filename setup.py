efdd151c7ee9b42a80aa5068e3740de04c3503f3
Stefan Heid <stefan-heid@web.de>
2021-04-22 20:41:36 +0200

    Added requirements to setup.py

--- setup.py
+++ setup.py
@@ -5,6 +5,8 @@ setup(
     name='PySplit',
     version=pysplit.__version__,
     packages=['pysplit', 'pysplit.client'],
+    install_requires=['requests>=2.20.0', 'PyYAML>=5.3.1',
+                      'Flask>=1.1.2', 'tornado~=5.1'],
     entry_points={
         'console_scripts': ['pysplit = pysplit.runner:main']
     }
